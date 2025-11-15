e3f7865ffa039ff348e9f1c9ca02ea003facb0c1
Thomas.Sutil <thomas.sutil@walibi.com>
2025-11-15 21:14:21 +0100

    COURS 5 - Finish

--- Thomas_Sutil_Valeriano/Bank.cs
+++ Thomas_Sutil_Valeriano/Bank.cs
@@ -1,8 +1,16 @@
 
+using System.Collections.Generic;
+
 public class Bank
 {
     public Dictionary<string, CurrentAccount> Accounts { get; } = new Dictionary<string, CurrentAccount>();
-    public required string Name { get; set; }
+    public string Name { get; set; }
+
+    // Constructor
+    public Bank (string name)
+    {
+        Name = name; 
+    }
 
     // Method for Add New Account
     public void AddAccount(CurrentAccount account)
@@ -11,32 +19,57 @@ public class Bank
         if (string.IsNullOrEmpty(account.Number))
         {
             Console.WriteLine("Number Account is empty");
+            return;
         }
         // Check if Number Account EXIST
         if (Accounts.ContainsKey(account.Number))
         {
             Console.WriteLine("This number account already exist");
+            return;
         }
         Accounts.Add(account.Number, account);
+        Console.WriteLine($"Account {account.Number} added successfully !");
     }
     // Method for Delete Account
-    public void DeleteAccount(string accountNumber)
+    public void DeleteAccount(CurrentAccount account)
     {
-        if (string.IsNullOrEmpty(accountNumber))
+        if (string.IsNullOrEmpty(account.Number))
         {
             Console.WriteLine("Number Account is empty");
         }
-        if (Accounts.ContainsKey(accountNumber))
+        if (Accounts.ContainsKey(account.Number))
         {
             Console.WriteLine("This number account already exist");
         }
-        Accounts.Remove(accountNumber);
+        Accounts.Remove(account.Number);
+        Console.WriteLine($"Account {account.Number} deleted successfully !");
     }
     // Method for return the Balance Account
 
     public double BalanceGet(CurrentAccount account)
     {
+        Console.WriteLine($"Total Balance for the {account.Number} : {account.Balance}");
         return account.Balance;
     }
 
+    // Method for return the SUM of Balance for all Account with the name
+    public double GetTotalBalanceAll(Person person)
+    {
+        if (person == null)
+        {
+            Console.WriteLine("I can't see ghosts !");
+            return 0;
+        }
+
+        double totalBalance = 0;
+        foreach (var account in Accounts.Values)
+        {
+            if (account.Owner == person)
+            {
+                totalBalance += account.Balance;
+            }
+        }
+        Console.WriteLine($"Total Balance for {person.FirstName} {person.LastName} : {totalBalance}");
+        return totalBalance;
+    }
 }
--- Thomas_Sutil_Valeriano/Program.cs
+++ Thomas_Sutil_Valeriano/Program.cs
@@ -1,8 +1,18 @@
-﻿Person p = new("Thomas", "Sutil", new DateTime(2001,3,25));
+﻿// Test for Person
+Person p = new("Thomas", "Sutil", new DateTime(2001,3,25));
 
 //Testing CurrentAccount Method
 CurrentAccount pA = new("BE3256", 1200, 0,p);
+CurrentAccount pB = new("BE32257", 1450, 0, p);
 pA.Withdraw(200);
 pA.Deposit(350);
+
+// Test for Bank
+Bank TotoBank = new("TotoBank");
+TotoBank.AddAccount(pA);
+TotoBank.AddAccount(pB);
+TotoBank.BalanceGet(pB);
+TotoBank.GetTotalBalanceAll(p);
+
 Console.WriteLine(p.FirstName + " " + p.LastName);
 Console.Write(pA.Number + " " + pA.Balance + " " + pA.CreditLine + " " + p.FirstName);
